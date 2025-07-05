c39bf0118eec043d2559e51f11ca7e52cef5d7f8
Đặng Hào Nguyên <103310320+danghaonguyen@users.noreply.github.com>
2025-07-05 18:52:37 +0700

    Update config.py

--- project/backend/config.py
+++ project/backend/config.py
@@ -1,9 +1,15 @@
 # config.py
-MYSQL_HOST = 'localhost'
-MYSQL_USER = 'root'
-MYSQL_PASSWORD = ''
-MYSQL_DB = 'diabetes_app'
-MYSQL_CURSORCLASS = 'DictCursor'
+# MYSQL_HOST = 'localhost'
+# MYSQL_USER = 'root'
+# MYSQL_PASSWORD = ''
+# MYSQL_DB = 'diabetes_app'
+# MYSQL_CURSORCLASS = 'DictCursor'
 
 """ SECRET_KEY = "your_super_secret_key"
  """
+
+MYSQL_HOST = 'sql12.freesqldatabase.com'
+MYSQL_USER = 'sql12788436'
+MYSQL_PASSWORD = 'KBmleBuMCe'
+MYSQL_DB = 'sql12788436'
+MYSQL_CURSORCLASS = 'DictCursor'
